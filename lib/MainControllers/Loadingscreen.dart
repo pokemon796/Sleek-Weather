c19e9d83c69d3de858002aa2b50020ffec0e8f6e
ozanmirza1 <ozan.mirza@icloud.com>
2020-02-15 13:19:53 -0500

    Fixing cause of rejection

--- lib/MainControllers/Loadingscreen.dart
+++ lib/MainControllers/Loadingscreen.dart
@@ -9,16 +9,39 @@ class Loadingscreen extends StatefulWidget {
 }
 
 class _Loadingscreen extends State<Loadingscreen> with TickerProviderStateMixin {
+  double display = 0.0;
   @override
   Widget build(BuildContext context) {
+    Future.delayed(const Duration(milliseconds: 5000), () {
+      setState(() {
+        display = 1.0;
+      });
+    });
     return Container(
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
+      child: Stack(
         children: <Widget>[
           Center(
             child: SpinKitRing(
               color: Colors.white,
               size: 75,
+            ),
+          ),
+          Positioned(
+            top: 35,
+            left: 0,
+            width: MediaQuery.of(context).size.width,
+            child: AnimatedOpacity(
+              duration: Duration(seconds: 2),
+              opacity: display,
+              child: Text(
+                "If it hasn't loaded, try rerunning the app.",
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  color: Colors.white,
+                  fontWeight: FontWeight.bold,
+                  fontSize: 15,
+                ),
+              ),
             ),
           ),
         ],
